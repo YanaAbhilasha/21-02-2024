907daaa9c7eb94c4d02dcb8d7687ca754e15854a
YanaAbhilasha <128958222+YanaAbhilasha@users.noreply.github.com>
2024-02-21 16:44:47 +0530

    Add files via upload

---
+++ terraform-projects/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.0"
+        }
+    }
+}
+provider "aws"{
+region = "us-east-1"
+access_key = "ASIA2E5COHMH3PPOEOFI"
+secret_key= "+SpBNQAKB+xMOPQEyqvdS6gEkoXOWiGjlZk6rDIb"
+token = "FwoGZXIvYXdzEGsaDMB3Cz0ms+7eoObQnSKvAbZvpiXHq4HK4OAQKfRCUsyIlKDC5hapYCMhAlJpqu4KO2ygJSoATFnMyAWcCCTVgD2I0p60A9JIhsZHWcc93IvmNUC0O7+1MsGiCwSqQZqbto96sDcVCAI8Ir+g3exkijp7rreMWTkgiet59jl9PxydxlEws7D46hMrOxucqeNGIPc3i2qh7d4pZwDt7ZeHNst1EeXgWcsuusqkUx5/lnOcAAXcox7QwqYLAvJNpX0ok4/XrgYyLSUyYBIkL91ESWveHEXjH5LS2AstNGZNm+vj+ouv+S1AHaMGIYoW4QhtAArWzA=="
+}
